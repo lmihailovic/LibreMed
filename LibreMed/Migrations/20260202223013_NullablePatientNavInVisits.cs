64e8ed49c004a309e588d6a930799f55a3b120ac
lmihailovic <miha.luka1006@gmail.com>
2026-02-02 23:30:41 +0100

    made patient nav property nullable in visits

---
+++ LibreMed/Migrations/20260202223013_NullablePatientNavInVisits.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LibreMed.Migrations
+{
+    /// <inheritdoc />
+    public partial class NullablePatientNavInVisits : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
